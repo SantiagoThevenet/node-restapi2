32b0b4f94f1fe3dba06aefdd5ab9b168191968df
santiago <santithevenetvalles@gmail.com>
2023-02-01 14:20:55 +0100

    conexion con mysql

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const pool = require('../db.js')
 
 /* GET home page. */
+router.get('/ping', async (req, res, next) => {
+  const [result] = await pool.query('SELECT 1 + 1 AS result')
+  res.json(result[0])
+})
+
 router.get('/', (req, res, next) => {
   res.send('<h1> :)</h1>')
 })
